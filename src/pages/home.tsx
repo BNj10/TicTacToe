bfec41cef50c072b5b47629384aeca24bc6731ba
DonyoBen <saquinbenjie7@gmail.com>
2025-09-19 05:43:02 +0800

    added home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+export default function Home()
+{   
+    return (
+        <div className=" text-white min-h-screen flex flex-col items-center justify-center space-y-6">
+           {/*GameBoard*/}
+            <p className="text-white font-montserrat text-shadow-lg"> Pick who goes first?</p>
+            {/*Buttons*/}
+        </div>
+    )
+}
