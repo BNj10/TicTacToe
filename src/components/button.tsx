d324fb7c74c126e2d507c485ff28d376f77c86e3
DonyoBen <saquinbenjie7@gmail.com>
2025-09-19 07:42:21 +0800

    refactor button code

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 export default function Button({value, onClick} : ButtonProps) {
     return (
         <button
-        className="text-black-800 rounded-xl shadow-lg h-25 w-25 p-5"
+        className="text-black-800 rounded-xl shadow-lg p-5 flex items-center justify-center text-4xl font-bold text-gray-700 border w-25 h-25 border-gray-300"
         onClick={() => onClick()}
     >
         {value}
