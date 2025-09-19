a3183675538c478a3a7423ce59e8bda88cf4f305
DonyoBen <saquinbenjie7@gmail.com>
2025-09-19 08:08:11 +0800

    added routes for different views

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/home"
+import Game from "./components/game"
+
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {<Route path="/game" element={<Game />} />}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
